2f719deb7f0dd3ee637b6679312553b9f8c63741
Yingying Lu <20095278@mail.wit.ie>
2024-01-02 22:48:12 +0000

    Add Heroku script to manifest file

---
+++ build/seedData/reviews.js
@@ -0,0 +1,29 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var reviews = [{
+  'movieId': 436270,
+  'author': 'user1',
+  'content': 'I like this movie',
+  'rating': 4,
+  'created_at': '21/12/2022',
+  'updated_at': '21/12/2022'
+}, {
+  'movieId': 436270,
+  'author': 'user2',
+  'content': 'I dont like this movie',
+  'rating': 1,
+  'created_at': '21/12/2022',
+  'updated_at': '21/12/2022'
+}, {
+  'movieId': 988233,
+  'author': 'user1',
+  'content': 'I love this movie',
+  'rating': 5,
+  'created_at': '20/12/2022',
+  'updated_at': '20/12/2022'
+}];
+var _default = exports["default"] = reviews;
--- build/seedData/users.js
+++ build/seedData/users.js
@@ -6,9 +6,11 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 var users = [{
   'username': 'user1',
-  'password': 'test1'
+  'password': "test123@",
+  'favourites': ['436270', '64690']
 }, {
   'username': 'user2',
-  'password': 'test2'
+  'password': "test123@",
+  'favourites': ['988233']
 }];
 var _default = exports["default"] = users;
